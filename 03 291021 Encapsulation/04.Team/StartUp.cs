b2266849eaa22e3041be6f38bdec06366b5c1a09
Stefan Bonev <bonev@x-spot.org>
2021-11-08 20:19:25 +0200

    03 291021

---
+++ 03 291021 Encapsulation/04.Team/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+
+            var team = new Team("SoftUni");
+            for (int i = 0; i < lines; i++)
+            {
+                var personArguments = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+
+                string firstName = personArguments[0];
+                string lastName = personArguments[1];
+                int age = int.Parse(personArguments[2]);
+                decimal salary = decimal.Parse(personArguments[3]);
+            }
+
+            foreach (Person person in persons)
+            {
+                team.AddPlayer(person);
+            }
+            Console.WriteLine(team);
+
+            //Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            //Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+        }
+    }
+}
---
+++ 03 291021 Encapsulation/04.Team/Team.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.Name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+        public string Name
+        {
+            get => name;
+            set { name = value; }
+        }
+        public IReadOnlyCollection<Person> FirstTeam
+        {
+            get { return this.firstTeam.AsReadOnly(); }
+        }
+
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get { return this.reserveTeam.AsReadOnly(); }
+        }
+
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"First team has {FirstTeam.Count} players.")
+              .AppendLine($"Reserve team has {ReserveTeam.Count} players.");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
