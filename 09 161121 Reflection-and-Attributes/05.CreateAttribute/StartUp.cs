a2c20703fa2c96fe641b21bfb8af4ce3b836c8e6
Stefan Bonev <bonev@x-spot.org>
2021-12-08 20:22:00 +0200

    09 161121

---
+++ 09 161121 Reflection-and-Attributes/05.CreateAttribute/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//first rename the project to AuthorProblem
+namespace AuthorProblem
+{
+    [Author("Anakin")]
+    public class StartUp
+    {
+        [Author("DartVaider")]
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
---
+++ 09 161121 Reflection-and-Attributes/06.CodeTracker/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeTracker
+{ //first rename the project to AuthorProblem
+    [Author("Anakin")]
+    public class StartUp
+    {
+        [Author("DartVayder")]
+        static void Main(string[] args)
+        {
+            Tracker tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
