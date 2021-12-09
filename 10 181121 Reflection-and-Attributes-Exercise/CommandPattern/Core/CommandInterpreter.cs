28e0f0ef5c442f7c44b6ade694b4425702b774d6
Stefan Bonev <bonev@x-spot.org>
2021-12-10 00:15:49 +0200

    10 181121

---
+++ 10 181121 Reflection-and-Attributes-Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Models
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] input = args.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string commandName = (input[0] + "Command").ToLower();
+
+            Type type = Assembly.GetCallingAssembly().GetTypes().FirstOrDefault(n => n.Name.ToLower() == commandName);
+            //Console.WriteLine((Assembly.GetCallingAssembly().GetTypes().FirstOrDefault(n => n.Name.ToLower() == commandName).Assembly.Get().Name));
+            ICommand instance = (ICommand)Activator.CreateInstance(type ?? throw new ArgumentException("Invalid command type!"));
+            string result = instance?.Execute(input.Skip(1).ToArray());
+            return result;
+        }
+    }
+}
