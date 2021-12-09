2ad047e9d4961cfbe91b87df8ae0280911dc17ad
Stefan Bonev <bonev@x-spot.org>
2021-12-10 00:07:03 +0200

    091221

---
+++ ExamPrep 091221 CSharp OOP Exam - 10 April 2021/01. Structure_Skeleton/AquaShop/Models/Aquariums/Aquarium.cs
@@ -0,0 +1,75 @@
+﻿using AquaShop.Models.Aquariums.Contracts;
+using AquaShop.Models.Decorations.Contracts;
+using AquaShop.Models.Fish.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AquaShop.Models.Aquariums
+{
+    public abstract class Aquarium : IAquarium
+    {
+        private string name;      
+        protected Aquarium(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.Fish = new List<IFish>();
+            this.Decorations = new List<IDecoration>();
+
+        }
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Aquarium name cannot be null or empty.");
+                }
+                this.name = value;
+            }
+        }
+
+        public int Capacity { get;}
+
+        public int Comfort => this.Decorations.Sum(d => d.Comfort);
+
+        public ICollection<IDecoration> Decorations { get; }
+
+        public ICollection<IFish> Fish { get; }
+
+        public void AddDecoration(IDecoration decoration)
+        {
+            this.Decorations.Add(decoration);
+        }
+        public void AddFish(IFish fish)
+        {
+            if (this.Fish.Count == this.Capacity)
+            {
+                throw new InvalidOperationException("Not enough capacity.");
+            }
+            this.Fish.Add(fish);
+        }
+
+        public void Feed()
+        {
+            foreach (var fish in this.Fish)
+            {
+                fish.Eat();
+            }
+        }
+
+        public string GetInfo()
+        {
+            return $"{this.name} ({this.GetType().Name}):{Environment.NewLine}" +
+                $"Fish: {(this.Fish.Count == 0 ? "none" : String.Join(", ", this.Fish))}{Environment.NewLine}" +
+                $"Decorations: {this.Decorations.Count}{Environment.NewLine}" +
+                $"Comfort: {this.Comfort}";
+        }
+
+        public bool RemoveFish(IFish fish)
+            => this.Fish.Remove(fish);
+    }
+}
---
+++ ExamPrep 091221 CSharp OOP Exam - 10 April 2021/03. Unit Tests_Skeleton/Aquariums.Tests/AquariumsTests.cs
@@ -0,0 +1,97 @@
+﻿namespace Aquariums.Tests
+{
+    using NUnit.Framework;
+    using System;
+
+    public class AquariumsTests
+    {
+        private Aquarium aquarium;
+        private Fish fish;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.aquarium = new Aquarium("Riverworld", 20);
+            this.fish = new Fish("Nemo");
+            this.aquarium.Add(this.fish);
+        }
+
+        [Test]
+        public void ValidAquariumName()
+        {
+            Assert.Throws<ArgumentNullException>(() => this.aquarium = new Aquarium(null, 20));
+            Assert.Throws<ArgumentNullException>(() => this.aquarium = new Aquarium(string.Empty, 20));
+        }
+
+        [Test]
+        public void Constructor_ShouldWorkProperly()
+        {
+            string expectedName = "Riverworld";
+            int expectedCapacity = 20;
+            Assert.AreEqual(expectedName, this.aquarium.Name);
+            Assert.AreEqual(expectedCapacity, this.aquarium.Capacity);
+        }
+
+        [Test]
+        public void CapacityShoudNotBeLessThenZero()
+        {
+            Assert.Throws<ArgumentException>(() => this.aquarium = new Aquarium("Riverworld", -1));       
+        }
+
+        [Test]
+        public void IfTheCapacityIsNotEnough()
+        {
+            this.aquarium = new Aquarium("Riverworld", 1);
+            this.aquarium.Add(this.fish);
+            Assert.Throws<InvalidOperationException>(() => this.aquarium.Add(this.fish));
+        }
+
+        [Test]
+        public void Count_ShoulWorkProperly()
+        {
+            Assert.AreEqual(1, this.aquarium.Count);
+        }
+
+        [Test]
+        public void AddShoudWorkProperly()
+        {
+            this.aquarium.Add(new Fish("Bambi"));
+            Assert.AreEqual(2, this.aquarium.Count);
+        }
+
+        [Test]
+        public void RemoveShoudWorkProperly()
+        {
+            this.aquarium.Add(new Fish("Bambi"));
+            this.aquarium.RemoveFish("Bambi");
+            Assert.AreEqual(1, this.aquarium.Count);
+        }
+
+        [Test]
+        public void ShoudThrowExeptionIfFishToRemoveIsNull()
+        {
+            Assert.Throws<InvalidOperationException>(() => this.aquarium.RemoveFish(null));
+        }
+
+        [Test]
+        public void ShoudThrowExeptionIfFishToSellisNull()
+        {
+            Assert.Throws<InvalidOperationException>(() => this.aquarium.SellFish(null));
+        }
+
+        [Test]
+        public void SellFish_ShouldWorkProperly()
+        {
+            Fish soldFish = this.aquarium.SellFish("Nemo");
+            Assert.IsFalse(soldFish.Available);
+        }
+
+        [Test]
+        public void Report_ShouldWorkProperly()
+        {
+            this.aquarium.Add(new Fish("Dori"));
+            string expected = $"Fish available at Riverworld: Nemo, Dori";
+            Assert.AreEqual(expected, this.aquarium.Report());
+        }
+    }
+}
